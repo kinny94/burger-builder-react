1c4369a43579fce53ff10a10b77cdd3881079549
kinny94 <ad.sam007@gmail.com>
2017-12-30 22:02:31 -0500

    Implementing Build Control

---
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import classes from './BuildControl.css';
+
+const buildControl = ( props ) => (
+    <div className={ classes.BuildControl }>
+        <div className={ classes.Label }>{ props.label }</div>
+
+        <button className={ classes.Less }>Less</button>
+        <button className={ classes.Less }>More</button>
+        
+    </div>
+);
+
+export default buildControl;
---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const buildControls = ( props ) => (
+    <div>
+        
+    </div>
+);
+
+export default buildControls;
