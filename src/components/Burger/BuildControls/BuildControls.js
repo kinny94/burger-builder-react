3641ee3c3af9c1a0c0ad3c90380b30aa69f0fec2
kinny94 <ad.sam007@gmail.com>
2017-12-30 22:08:28 -0500

    Implementing buildcontrols 2

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import BuildControl from './BuildControl/BuildControl';
+
+import classes from './BuildControls.css';
+
+const controls = [
+    { label: 'Salad', type: 'salad' },
+    { label: 'Bacon', type: 'bacon' },
+    { label: 'Cheese', type: 'cheese' },
+    { label: 'Meat', type: 'meat' }
+];
 
 const buildControls = ( props ) => (
-    <div>
-        
+    <div className={ classes.BuildControls }>
+        {controls.map(ctrl => (
+            <BuildControl key={ ctrl.label } label={ ctrl.label } />
+        ))}
     </div>
 );
 
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Aux from '../../hoc/Auxilary';
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
 class BurgerBuilder extends Component{
 
@@ -20,7 +21,7 @@ class BurgerBuilder extends Component{
         return (
             <Aux>
                 <Burger ingridients={ this.state.ingridients }/>
-                <div>Build Controls</div>
+                <BuildControls />
             </Aux>
         );
     }
