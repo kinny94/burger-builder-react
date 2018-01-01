cb0d7172699bf9a145bc69a312d8f2f3391e4a93
kinny94 <ad.sam007@gmail.com>
2017-12-31 20:18:07 -0500

    Implemented Logo and styling

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import classes from './Toolbar.css'
+import Logo from '../../Logo/Logo';
 
 const toolbar = ( props ) => (
     <header className={ classes.Toolbar }>
         <div>MENU</div>
-        <div>LOGO</div>
+        <Logo />
         <nav>
             ...
         </nav>
