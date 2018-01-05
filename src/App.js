c0ac9dbfb17255f63272a2d8793c1595e599b726
kinny94 <ad.sam007@gmail.com>
2018-01-04 22:57:48 -0500

    Implemented Navigation Links with routing

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
+import Orders from './containers/Orders/Orders';
 
 class App extends Component {
 
@@ -11,8 +12,9 @@ class App extends Component {
 			<div>
 				<Layout>
 					<Switch>
-						<Route path="/checkout" component={Checkout} />
-						<Route path="/" exact component={BurgerBuilder} /> 
+						<Route path="/checkout" component={ Checkout } />
+						<Route path="/orders" component={ Orders } />
+						<Route path="/" exact component={ BurgerBuilder } /> 
 					</Switch>
 				
 				</Layout>
--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import classes from './NavigationItem.css';
+import { NavLink } from 'react-router-dom';
 
 const navigationItem = ( props ) => (
-    <li className={ classes.NavigationItem }><a 
-        href={ props.link } 
-        className={ props.active ? classes.active : null} >{ props.children }</a></li>
+    <li className={ classes.NavigationItem }><NavLink 
+        exact={ props.exact }
+        activeClassName={ classes.active }
+        to={ props.link } 
+        >{ props.children }</NavLink></li>
 );
 
 export default navigationItem;
