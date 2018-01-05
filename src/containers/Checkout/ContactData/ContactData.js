e0dc05f4942553534e26d8684a2f939d646a5f6e
kinny94 <ad.sam007@gmail.com>
2018-01-05 18:28:40 -0500

    Creating dynamic fields

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -91,12 +91,25 @@ class ContactData extends Component{
 
     render(){
 
+        const formElementArray = [];
+
+        for( let key in this.state.orderForm ){
+            formElementArray.push({
+                id: key,
+                config: this.state.orderForm[key]
+            });
+        }
+
         let form = (
             <form>  
-                <Input elementType="..." elementConfig="..." value="..." />
-                <Input inputType="input" type="email" name="email" placeholder="Your Email" />
-                <Input inputType="input" type="text" name="street" placeholder="Street" />
-                <Input inputType="input" type="text" name="postal" placeholder="Postal Code" />
+                { formElementArray.map(formElement => (
+                    <Input 
+                        key={ formElement.id }
+                        elementType={ formElement.config.elementType }
+                        elementConfig={ formElement.config.elementConfig }
+                        value={ formElement.config.value }
+                    />
+                ))}
                 <Button btnType="Success" clicked={ this.orderHandler } >ORDER</Button>
             </form>
         );
