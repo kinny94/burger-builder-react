4651d319e5db3f02f4f3cab3a0342d5c62b366d5
kinny94 <ad.sam007@gmail.com>
2018-01-05 19:07:54 -0500

    Added form Validation

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -15,7 +15,11 @@ class ContactData extends Component{
                     type: 'text',
                     placeholder: 'Your Name'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false
             },
             street: {
                 elementType: 'input',
@@ -23,7 +27,11 @@ class ContactData extends Component{
                     type: 'text',
                     placeholder: 'Your Street'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false
             },
             zipCode: {
                 elementType: 'input',
@@ -31,7 +39,13 @@ class ContactData extends Component{
                     type: 'text',
                     placeholder: 'Your zip code'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true,
+                    minLength: 5,
+                    maxLength: 5
+                },
+                valid: false
             },
             country: {
                 elementType: 'input',
@@ -39,7 +53,11 @@ class ContactData extends Component{
                     type: 'text',
                     placeholder: 'Your Country'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false
             },
             email: {
                 elementType: 'input',
@@ -47,7 +65,11 @@ class ContactData extends Component{
                     type: 'email',
                     placeholder: 'Your Email'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false
             },
             deliveryMethod: {
                 elementType: 'select',
@@ -95,6 +117,24 @@ class ContactData extends Component{
 
     }
 
+    checkValidation( value, rules ){
+        
+        let isValid = true;
+        if( rules.required ){
+            isValid = value.trim() !== ''  ;
+        }
+
+        if( rules.minLength ){
+            isValid = value.length >= rules.minLength && isValid;
+        }
+
+        if( rules.maxLength ){
+            isValid = value.length <= rules.maxLength && isValid;
+        }
+
+        return isValid;
+    }
+
     inputChangedHandler = (e, inputIdentifier) =>{
         //e.preventDefault();
         const updatedOrderForm = {
@@ -106,7 +146,10 @@ class ContactData extends Component{
         };
 
         updatedFormElement.value = e.target.value;
+        updatedFormElement.valid = this.checkValidation( updatedFormElement.value, updatedFormElement.validation );
         updatedOrderForm[inputIdentifier] = updatedFormElement;
+        
+        console.log( updatedFormElement );
         this.setState({
             orderForm: updatedOrderForm
         });
