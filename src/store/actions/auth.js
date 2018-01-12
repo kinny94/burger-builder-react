94128be4bb78dae22a942a9c6421b918b62e5c05
kinny94 <ad.sam007@gmail.com>
2018-01-11 19:42:46 -0500

    Adding logout functionality

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -22,6 +22,20 @@ export const authFailure = ( error ) => {
     };
 };
 
+export const checkAuthTimeout = ( expirationTime ) => {
+    return dispatch => {
+        setTimeout(() => {
+            dispatch( logout() ); 
+        }, expirationTime * 1000);
+    };
+};
+
+export const logout = () => {
+    return {
+        type: actionTypes.AUTH_LOGOUT
+    };
+};
+
 export const auth = ( email, password, isSignup ) => {
     return dispatch => {
         
@@ -43,6 +57,7 @@ export const auth = ( email, password, isSignup ) => {
         .then( response => {
             console.log( response );
             dispatch( authSuccess( response.data.idToken, response.data.localId ) );
+            dispatch( checkAuthTimeout( response.data.expiresIn ) );
         })
         .catch(err => {
             dispatch( authFailure( err.response.data.error ) );
--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -28,11 +28,16 @@ const authFail = ( state, action ) => {
     });
 };
 
+const authLogout = ( state, action ) => {
+    return updateObject( state, { token: null, userId: null });
+};
+
 const reducer = ( state = initialState, action ) => {
     switch( action.type ) {
         case actionTypes.AUTH_START: return authStart( state, action );
         case actionTypes.AUTH_SUCCESS: return authSuccess( state, action );
         case actionTypes.AUTH_FAIL: return authFail( state, action );
+        case actionTypes.AUTH_LOGOUT: return authLogout( state, action );
         default: return state;
     }
 };
